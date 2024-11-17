26c48de13d6cbc03c461620c03dbde8c549b01f0
Faisal Khrisan <150114510+faisal-khrisan@users.noreply.github.com>
2024-11-17 10:43:44 +0800

    Add files via upload

---
+++ even and odd number checker.py
@@ -0,0 +1,11 @@
+print ("Welcome to our program.\nOur is designed to check whether the number is even or odd")
+k="y"
+while k=="y" or k== "Y" :
+    num= int(input ("please insert a number to check: "))
+    if num%2==0:
+        print (f"The number you entered {num} is an even number")
+    else :
+        print (f"The number you entered {num} is an odd number")
+    k=input("Would you like to continue? y / n ")
+    print ("###################################################################")
+print("Thank you for using our checking app")
